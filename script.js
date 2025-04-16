bcb457f18b5d57cd183f34fc93591af6574a1a9c
bea <bea>
2025-04-16 20:30:50 +0200

    zwei spalten hinzugefügzwei spalten hinzugefügtt

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function changeText() {
     document.querySelector('h1').textContent = 'Hallo, JavaScript!';
 }
 
+function aktualisiereVorschau() {
+    const eingabe = document.getElementById("eingabeFeld").value;
+    document.getElementById("vorschauFeld").innerHTML = eingabe.replace(/\n/g, "<br>");
+  }
